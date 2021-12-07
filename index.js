a232b7c14636888265961cb23069fc9272c39402
Anonno Ruddho <ahmedrimon377472@gmail.com>
2021-12-07 19:57:52 +0600

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Thank you for calling me')
+})
+
+app.listen(3000, () => console.log('Listening port on 3000'))
